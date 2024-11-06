6b95f46dbce063a1744b86d3ecf1c14d2522a5ea
César Guillermo Nevárez <cesarguillermonevarez@MacBook-Pro-de-Cesar.local>
2024-11-06 09:48:39 +0000

    add comments component, modal window opens when clicking ViewComments

--- fe-nc-news/src/Components/ArticleDetails.jsx
+++ fe-nc-news/src/Components/ArticleDetails.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { apiClient } from "../utils/APIclient";
 import { ArticleDetailsCard } from "./ArticleDetailsCard";
+import { Comments } from "./Comments";
+Comments;
 export const ArticleDetails = () => {
   const { article_id } = useParams();
   const [articleDetails, setArticleDetails] = useState({});
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     apiClient.get(`/articles/${article_id}`).then((response) => {
@@ -12,9 +15,15 @@ export const ArticleDetails = () => {
     });
   }, []);
 
+  useEffect(() => {
+    apiClient.get(`/articles/${article_id}/comments`).then((response) => {
+      setComments(response.data.comments);
+    });
+  }, []);
+
   return (
     <>
-      <ArticleDetailsCard articleDetails={articleDetails} />
+      <ArticleDetailsCard articleDetails={articleDetails} comments={comments} />
     </>
   );
 };
